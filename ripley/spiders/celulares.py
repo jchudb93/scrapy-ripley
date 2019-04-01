fd86e62ad2575edf9241e06f941e5776b11b1c0a
jose chu <jchudb93@gmail.com>
2019-04-01 18:07:39 -0500

    celulares

---
+++ ripley/spiders/celulares.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from ripley.items import RipleyItemLoader
+from scrapy import Spider, Request
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+
+import ripley.utils as utils
+
+class CelularSpider(scrapy.Spider):
+    name = 'celular'
+    allowed_domains = ['simple.ripley.com.pe']
+    start_urls = ['https://simple.ripley.com.pe/celulares/smartphones/todo-celulares?source=menu'
+        ]
+
+    
+    def parse(self, response):
+
+        for r in self.parse_items(response):
+            yield r
+        
+        n_paginas = len(response.xpath('//div[has-class("catalog-page__footer-pagination")]//nav/ul[has-class("pagination")]//li').extract()) - 2
+        
+        for pagina in range(2, n_paginas + 1):
+
+            url = f'{response.url}&page={pagina}'
+            yield Request(url=url, callback=self.parse_items)
+
+    def parse_items(self, response):
+
+        tipo_producto = utils.obtener_sub_categoria_str(response.url)
+        sub_categoria = 'celulares'
+        categoria = 'celulares'
+        item_xpath = '//div//a[has-class("catalog-product-item catalog-product-item__container col-xs-6 col-sm-6 col-md-4 col-lg-4")]'
+        # //*[@id="catalog-page"]/div/div[2]/div[3]/section/div/div/a[1]
+        for producto in response.xpath(item_xpath):
+            ripley_item_loader = RipleyItemLoader(response=response)
+            marca = producto.xpath('.//div[has-class("brand-logo")]/span/text()').extract()
+            nombre = producto.xpath('.//div[has-class("catalog-product-details__name")]/text()').extract()
+            precio = producto.xpath('.//div[has-class("catalog-prices")]/ul/li/text()').extract_first()
+            url_imagen = producto.xpath('.//div[has-class("images-preview")]//img/@data-src').extract_first()
+            ripley_item_loader.add_value('marca', marca)
+            ripley_item_loader.add_value('nombre', nombre)
+            ripley_item_loader.add_value('precio', precio)
+            ripley_item_loader.add_value('imagen', url_imagen)
+            ripley_item_loader.add_value('descripcion', '')
+            ripley_item_loader.add_value('tipo_producto', tipo_producto)
+            ripley_item_loader.add_value('sub_categoria', sub_categoria)
+            ripley_item_loader.add_value('categoria', categoria)
+            ripley_item_loader.add_value('url', str(response.url))
+        
+            yield ripley_item_loader.load_item()
+
