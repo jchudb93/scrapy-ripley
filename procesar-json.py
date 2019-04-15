6fd2f35a93aa4259ae4d2d8b7242d7c421115879
jose chu <jchudb93@gmail.com>
2019-04-15 15:09:33 -0500

    procesar json

--- procesar-json.py
+++ procesar-json.py
@@ -6,8 +6,8 @@ import io
 def nombrar_archivo(archivo):
     nombre = ''.join(archivo.split('.')[:-1]).split('\\')[-1]
     return nombre
-    
-def main():
+
+def procesar_archivos():
     archivos = glob.glob('./*.json')
     for archivo in archivos:
         
@@ -18,7 +18,20 @@ def main():
         df['precio'] = df['precio'].astype(float)
         gp = df.groupby(['categoria','tipo_producto']).agg({'precio': 'mean'}).reset_index()
         nombre_archivo = nombrar_archivo(archivo)
-        gp.to_csv(f'./{nombre_archivo}.csv', encoding='utf8')
+        gp.to_csv(f'./productos/{nombre_archivo}.csv', encoding='utf8', index=False)
         
+def generar_json():
+    archivos = glob.glob('./productos/*.csv')
+    lst_df = [pd.read_csv(archivo) for archivo in archivos]
+    df_final = pd.concat(lst_df)
+    df_final = df_final.reset_index(drop=True)
+    df_final['id'] = range(1, len(df_final) + 1)
+    cols = df_final.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    df_final = df_final[cols]
+    df_final.to_json('productos.json', orient='record')
+
+def main():
+    procesar_archivos()
 if __name__ == '__main__':
     main()
